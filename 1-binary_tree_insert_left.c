2ed2afef83b828cf2afeaedd51e8a6dc514c9a1c
Ian Kerich <iankerich254@gmail.com>
2024-06-06 20:19:08 +0300

    Tasks 1 to 5

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Inserts a node as the left-child of another node
+ * @parent: Pointer to the node to insert the left-child in
+ * @value: Value to store in the new node
+ *
+ * Return: Pointer to the created node, or NULL on failure or if parent is NULL
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node_ker;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new_node_ker = binary_tree_node(parent, value);
+	if (new_node_ker == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
+	{
+		new_node_ker->left = parent->left;
+		parent->left->parent = new_node_ker;
+	}
+	parent->left = new_node_ker;
+
+	return (new_node_ker);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -2,6 +2,7 @@
 #define BINARY_TREES_H
 
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -24,7 +25,15 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-/* Function prototype */
+/* Function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
+
+/* Function for visualizing the tree (not provided here) */
+void binary_tree_print(const binary_tree_t *);
 
 #endif /* BINARY_TREES_H */
